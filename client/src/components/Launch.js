8be876d661f2989278ad94f63fdb0956b9f4e566
khoa <54516445+khoatransjsu@users.noreply.github.com>
2021-09-08 12:11:34 -0700

    client update

---
+++ client/src/components/Launch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+export default class Launch extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
