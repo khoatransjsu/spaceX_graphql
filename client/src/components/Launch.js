1b27d66a0ee84e2d92db10fc4906b835eed2dbce
khoa <54516445+khoatransjsu@users.noreply.github.com>
2021-09-08 12:25:33 -0700

    router&launch query updated

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import {Link} from 'react-router-dom';
 
-export default class Launch extends Component {
+const LAUNCHES_QUERY = gql`   
+    query LaunchQuery($flight_number: Int!){
+            launch(flight_number: $flight_number){
+                flight_number,
+                mission_name,
+                launch_year,
+                launch_success,
+                launch_year,
+                rocket{
+                    rocket_id,
+                    rocket_name,
+                    rocket_type
+                }
+            }
+        }     
+    `;
+
+export default class Launch extends Component {   
     render() {
+        let {flight_number} = this.props.match.params;
+        flight_number =parseInt(flight_number);
         return (
-            <div>
-                
-            </div>
+            <>
+                <Query query = {LAUNCHES_QUERY}>
+
+                </Query>
+            </>
         )
     }
 }
